fca2e422fb93aec20a8c3a707733fc028834a8fd
a_ootani <a_ootani@LAPTOP-VTVE7UO6>
2026-06-25 11:24:44 +0900

    sun

---
+++ src/main/java/bean/Order.java
@@ -0,0 +1,66 @@
+package bean;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String order_id;
+	private String table_id;
+	private String status;
+	private String total_amount;
+	private String payment_method;
+	private LocalDateTime ordered_at;
+	
+	public Order() {}
+	
+	public String getOrder_id() {
+		return order_id;
+	}
+	
+	public String getTable_id() {
+		return table_id;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public String getTotal_amount() {
+		return total_amount;
+	}
+	
+	public String getPayment_method() {
+		return payment_method;
+	}
+	
+	public LocalDateTime getOrdered_at() {
+		return ordered_at;
+	}
+	
+	public void setOrder_id(String order_id) {
+		this.order_id = order_id;
+	}
+
+	public void setTable_id(String table_id) {
+		this.table_id = table_id;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void setTotal_amount(String total_amount) {
+		this.total_amount = total_amount;
+	}
+
+	public void setPayment_method(String payment_method) {
+		this.payment_method = payment_method;
+	}
+
+	public void setOrdered_at(LocalDateTime ordered_at) {
+		this.ordered_at = ordered_at;
+	}
+	
+}
