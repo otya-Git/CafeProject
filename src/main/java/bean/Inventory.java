bd590c8cad7f05533078c35dfd074e421847fa30
sunsun0924 <hrs2521428@stu.o-hara.ac.jp>
2026-06-25 11:16:34 +0900

    sunsun

---
+++ src/main/java/bean/Inventory.java
@@ -0,0 +1,85 @@
+package bean;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Inventory {
+	private long inventoryId;
+	private long productId;
+	private long supplierId;
+	private double stockQuantity;
+	private String unit;
+	private double reorderPoint;
+	private Date expiryDate;
+	private Timestamp updatedAt;
+	
+	public Inventory() {
+	}
+	
+	public long getInventoryId() {
+		return inventoryId;
+	}
+	
+	public void setInventoryId(long inventoryId) {
+		this.inventoryId = inventoryId;
+	}
+	
+	public long getproductId() {
+		return productId;
+	}
+	
+	public void setproductId(long productId) {
+		this.productId = productId;
+	}
+	
+	public long getSupplierId() {
+		return supplierId;
+	}
+	
+	public void getSupplierId(long supplierId) {
+		this.supplierId = supplierId;
+	}
+	
+	public double getStockQuantity() {
+		return stockQuantity;
+	}
+	
+	public void setStockQuantity(double stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+	
+	public String getUnit() {
+		return unit;
+	}
+	
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	public double getReorderPoint() {
+		return reorderPoint;
+	}
+	
+	public void setReorderPoint(double reorderPoint) {
+		this.reorderPoint = reorderPoint;
+	}
+	
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+	
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+
+}
