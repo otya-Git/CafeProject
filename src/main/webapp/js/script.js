1a7158a7ed00cdf94bf1fe6254c631e51a600448
KENTY <hrs2521452@stu.o-hara.ac.jp>
2026-07-13 10:22:01 +0900

    ハンバーガーメニューが！！なんと！！更に！！綺麗になりました！！

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -2,9 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('js-hamburger');
     const nav = document.getElementById('js-nav');
 
+    // パディングを含めて高さを計算し、画面内に収めてスクロール可能にする
+    nav.style.boxSizing = 'border-box';
+    nav.style.height = '100vh';
+    nav.style.maxHeight = '100vh';
+    nav.style.overflowY = 'auto';
+
     hamburger.addEventListener('click', function() {
-        // ボタンとメニューに「active」クラスをつけ外しする
         hamburger.classList.toggle('active');
         nav.classList.toggle('active');
+
+        if (nav.classList.contains('active')) {
+            // ▼【ガタつき防止】スクロールバーが消える分の「ガタッ」という動きを相殺する
+            const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+            document.body.style.paddingRight = `${scrollbarWidth}px`;
+            
+            // 後ろのページを固定
+            document.body.style.overflow = 'hidden';
+        } else {
+            // ▼【元の状態に戻す】
+            document.body.style.paddingRight = '';
+            document.body.style.overflow = '';
+        }
     });
 });
